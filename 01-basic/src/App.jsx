b6fc352e2a8ff960d91112e174a58682f2b9fdac
Nanda Bendkoli <bendkolinanda12@gmail.com>
2026-03-18 22:57:26 +0530

    1st commit

---
+++ 01-basic/src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+  return (
+    <>
+      <h1>Hello i am nanda i am from nashik maharashtra!</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut repudiandae excepturi totam.</p>
+
+    </>
+  );
+}
+
+export default App;
